c8a40ba1b00d5e9b9c26adfb328ccecd078db189
Donavyn <donavynhaley@gmail.com>
2023-12-04 16:38:01 -0800

    go init

---
+++ cmd/day01/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/donavynhaley/advent-of-code-2023/pkg/day01"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	input, err := os.ReadFile(filepath.Join("..", "..", "input", "day01.txt"))
+	if err != nil {
+		log.Fatalf("Failed to read input file: %v", err)
+	}
+
+	result := day01.SolvePartOne(string(input))
+	fmt.Printf("Part 1 Input: %v\n", string(input))
+	fmt.Printf("Part 1 Result: %v\n", result)
+
+	result = day01.SolvePartTwo(string(input))
+	fmt.Printf("Part 2 Input: %v\n", string(input))
+	fmt.Printf("Part 2 Result: %v\n", result)
+}
---
+++ pkg/day01/day01.go
@@ -0,0 +1,20 @@
+package day01
+
+// SolvePartOne is a function that processes the input for the first part of the day's challenge.
+// It returns the result as an appropriate type (int, string, etc.) based on the problem's requirements.
+func SolvePartOne(input string) int {
+	// Implement the logic for Part 1 of Day 1's challenge here.
+	// Convert or process the input as needed.
+	// Return the result as per the challenge's requirements.
+	// For example, return a dummy result like 0 for now.
+	return 0
+}
+
+// SolvePartTwo is similar to SolvePartOne, but for the second part of the day's challenge.
+func SolvePartTwo(input string) int {
+	// Implement the logic for Part 2 of Day 1's challenge here.
+	// The implementation may be different from Part 1 based on the problem's requirements.
+	// Return the final result.
+	// For example, return a dummy result like 0 for now.
+	return 0
+}
